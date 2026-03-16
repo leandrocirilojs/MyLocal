a96cffdc15bd97fd82633509f041f7b2980a256f
leandrocirilojs <48395196+leandrocirilojs@users.noreply.github.com>
2026-03-15 21:48:39 -0300

    Update config.js

--- config.js
+++ config.js
@@ -11,16 +11,6 @@ const firebaseConfig = {
   storageBucket: "mylocal-578a0.firebasestorage.app",
   messagingSenderId: "536709482770",
   appId: "1:536709482770:web:6ff8ee22d4f48b0a7b4bcb"
+  // Seu email Google — só este email vê o painel "Todos os Usuários"
+  adminEmail: "fleandrocirilopeixoto@gmail.com "
 };
-
-/* Exemplo do seu projeto (baseado no que vimos nas screenshots):
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyCl4f1jEq-kM-XIGtGL_VOnGsGezFN85jY",
-  authDomain:        "mylocal-578a0.firebaseapp.com",
-  databaseURL:       "https://mylocal-578a0-default-rtdb.firebaseio.com",
-  projectId:         "mylocal-578a0",
-  storageBucket:     "mylocal-578a0.firebasestorage.app",
-  messagingSenderId: "536709482770",
-  appId:             "1:536709482770:web:6ff8ee22d4f48b0a7b4bcb"
-};
-*/
